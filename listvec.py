eb569de6068eb1170b59ab2f5b096a36f2f0e452
Eduardo Pavez <eduardo.pavez@uc.cl>
2022-10-24 21:48:01 -0300

    corrección de listvec

--- listvec.py
+++ listvec.py
@@ -7,9 +7,9 @@ class ListVec(list):
 
     def vec_sum(self, vec):
 
-        assert len(self) == len(vec)
+        assert len(self.e) == len(vec.e)
         lista_nueva = []
-        for i in range(len(self)):
+        for i in range(len(self.e)):
             lista_nueva.append(self.e[i] + vec.e[i])
 
         return ListVec(lista_nueva)
@@ -25,7 +25,7 @@ class ListVec(list):
 
         assert len(self) == len(vec)
         dot = 0
-        for i in range(len(self)):
+        for i in range(len(self.e)):
             dot += self.e[i] * vec.e[i]
 
         return dot
@@ -33,14 +33,14 @@ class ListVec(list):
     def norm(self):
         sum = 0
         for e in self.e:
-            norma += e**2
+            sum += e**2
 
         return sum**(1/2)
 
     def similarity(self, b):
 
         assert len(self) == len(b)
-        for i in range(len(self)):
+        for i in range(len(self.e)):
             if self.e[i] == b.e[i]:
                 pass
             else:
